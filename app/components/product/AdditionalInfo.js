f342ac01fba99281de13abd7d749e66d11c3425e
siam chowdhury <smsiam696@gmail.com>
2024-08-29 01:54:21 +0600

    bug fix

--- app/components/product/AdditionalInfo.js
+++ app/components/product/AdditionalInfo.js
@@ -4,7 +4,7 @@ import { Table } from "@mantine/core";
 const AdditionalInfo = ({ product = null }) => {
   return (
     <div className="sm:px-4 py-6">
-      <Table captionSide="bottom" highlightOnHover withColumnBorders>
+      <Table captionSide="bottom" highlightOnHover withBorder withColumnBorders>
         <tbody>
           <tr key={"aaaa"}>
             <td>ওজন</td>
