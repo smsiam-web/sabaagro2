62135a7f1a7de596d6c0b9ca8aea5da142aa4b85
siam chowdhury <smsiam696@gmail.com>
2024-08-29 02:15:39 +0600

    update

--- pages/admin/index.js
+++ pages/admin/index.js
@@ -1,51 +1,26 @@
-import React, {useEffect} from "react";
+import React, { useEffect } from "react";
 import AdminLayout from "@/admin/AdminLayout";
 import DashBoard from "@/admin/components/dashboard";
 import { useDispatch } from "react-redux";
 import { db } from "@/app/utils/firebase";
 import { updateConfig } from "@/app/redux/slices/configSlice";
-import { updateOrder } from "@/app/redux/slices/orderSlice";
 
 const AdmainMain = () => {
-
   const dispatch = useDispatch();
 
-    //get config
-    useEffect(() => {
-      const unSub = db.collection("config").onSnapshot((snap) => {
-        const configData = [];
-        snap.docs.map((doc) => {
-          configData.push(
-            doc.data()
-          )
-        });
-        dispatch(updateConfig(configData));
+  //get config
+  useEffect(() => {
+    const unSub = db.collection("config").onSnapshot((snap) => {
+      const configData = [];
+      snap.docs.map((doc) => {
+        configData.push(doc.data());
       });
-      return () => {
-        unSub();
-      };
-    }, []);
-
-  //     // Get order from firebase database
-  // useEffect(() => {
-  //   const unSub = db
-  //     .collection("placeOrder")
-  //     .orderBy("timestamp", "desc")
-  //     .onSnapshot((snap) => {
-  //       const order = [];
-  //       snap.docs.map((doc) => {
-  //         order.push({
-  //           id: doc.id,
-  //           ...doc.data(),
-  //           // timestamp: doc.data().timestamp?.toDate().getTime(),
-  //         });
-  //       });
-  //       dispatch(updateOrder(order));
-  //     });
-  //   return () => {
-  //     unSub();
-  //   };
-  // }, []);
+      dispatch(updateConfig(configData));
+    });
+    return () => {
+      unSub();
+    };
+  }, []);
 
   return (
     <AdminLayout>
