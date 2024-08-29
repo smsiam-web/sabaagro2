fcfa1295d53c51c0db7fb053c978ff300a0484a8
siam chowdhury <smsiam696@gmail.com>
2024-08-29 14:32:29 +0600

    invoice up

--- admin/components/placeOrder/singleOrder/index.js
+++ admin/components/placeOrder/singleOrder/index.js
@@ -74,32 +74,35 @@ const OrderDetails = ({ onClick }) => {
         />
         {/* <GenerateStick html={ref} /> */}
       </div>
-      <div className="w-full relative aspect-[1/1.414] bg-white" ref={ref}>
+      <div
+        className="w-full relative aspect-[1/1.7] sm:aspect-[1/1.414] bg-white"
+        ref={ref}
+      >
         <div>
           <div ref={ref}>
             <div className="flex flex-col w-full gap-2">
               <img
                 src="/invoice/saba_head.jpg"
                 alt=""
-                className="w-3/5 pl-4 pt-2 sm:pl-8 sm:pt-6 sm:pb-2"
+                className="w-[45%] pl-4 pt-2 sm:pl-8 sm:pt-6 sm:pb-2"
               />
               <img
                 src="/invoice/saba_bar.png"
                 alt=""
-                className="w-2/3 self-end"
+                className="w-[60%] self-end"
               />
             </div>
             <div className="px-4 sm:px-10 font-mono">
               <div>
                 <div className="flex justify-between items-center pb-1 sm:pb-2 sm:pt-1">
-                  <div className="w-1/2">
+                  <div className="w-[38%]">
                     <img id="bar_code" className="w-full" ref={inputRef} />
                     <span id="invoiceNo" className="hidden">
                       {id}
                     </span>
                   </div>
                   <div className="w-full flex justify-end items-end text-end">
-                    <div class="text-xs sm:text-xl lg:text-2xl font-semibold flex-column gap-1 mt-2">
+                    <div class="text-xs md:text-base lg:text-2xl font-semibold flex-column gap-1 mt-2">
                       <p>
                         Date:{" "}
                         <span className="font-medium">{singleOrder?.date}</span>
@@ -126,10 +129,10 @@ const OrderDetails = ({ onClick }) => {
                   </div>
                 </div>
                 <div className="sm:mb-4">
-                  <h1 className="text-title text-md md:text-2xl font-semibold border-b sm:border-b-2 sm:pb-1">
+                  <h1 className="text-title text-md md:text-xl font-semibold border-b sm:border-b-2 sm:pb-1">
                     Customer Details:
                   </h1>
-                  <div class="text-sm sm:text-xl md:text-2xl font-semibold flex-column sm:gap-1 sm:mt-2">
+                  <div class="text-sm sm:text-base md:text-xl font-semibold flex-column sm:gap-1 sm:mt-2">
                     <p className="text-title">
                       Name :{" "}
                       <span className="font-medium">
@@ -151,11 +154,11 @@ const OrderDetails = ({ onClick }) => {
                   </div>
                 </div>
                 <div>
-                  <h1 className="text-title text-md sm:text-2xl md:text-3xl font-semibold border-b sm:border-b-2">
+                  <h1 className="text-title text-md md:text-xl font-semibold border-b sm:border-b-2">
                     Order Details:
                   </h1>
                   <table className="w-full whitespace-nowrap table-auto border border-gray-100">
-                    <thead className="text-xs sm:text-base font-semibold tracking-wide text-left  uppercase bg-zinc-800 border-slate-800 border text-slate-50">
+                    <thead className="text-[10px]  sm:text-sm font-semibold tracking-wide text-left  uppercase bg-zinc-800 border-slate-800 border text-slate-50">
                       <tr>
                         <th className="px-2 sm:px-4 sm:py-1">SL</th>
                         <th className="px-2 sm:px-4 sm:py-1">Item</th>
@@ -173,38 +176,40 @@ const OrderDetails = ({ onClick }) => {
                           singleOrder.order.map((item, i) => (
                             <tr
                               key={i}
-                              className={`${(i + 1) % 2 == 0 && "bg-sub"} px-2`}
+                              className={`${
+                                (i + 1) % 2 == 0 && "bg-sub"
+                              } leading-tight px-2`}
                             >
                               <td className="px-2 sm:px-4 sm:py-1 font-medium">
-                                <span className="text-xs sm:text-base">{`0${
+                                <span className="text-[10px] sm:text-sm">{`0${
                                   i + 1
                                 }.`}</span>
                               </td>
                               <td className="px-2 sm:px-4 sm:py-1 font-medium">
-                                <span className="text-xs sm:text-base">
+                                <span className="text-[10px] sm:text-sm">
                                   {item.product_name}
                                 </span>
                               </td>
 
                               <td className="px-2 sm:px-4 sm:py-1 font-medium">
-                                <span className="text-xs sm:text-base">
+                                <span className="text-[10px] sm:text-sm">
                                   {item.child_category}
                                 </span>
                               </td>
 
                               <td className="px-2 sm:px-4 sm:py-1">
-                                <span className="text-xs sm:text-base font-semibold">
+                                <span className="text-[10px] sm:text-sm font-semibold">
                                   {item.quantity}
                                   {item.unit}
                                 </span>
                               </td>
                               <td className="px-2 sm:px-4 sm:py-1">
-                                <span className="text-xs sm:text-base font-semibold">
+                                <span className="text-[10px] sm:text-sm font-semibold">
                                   {item.sale_price}
                                 </span>
                               </td>
                               <td className="px-2 sm:px-4 sm:py-1">
-                                <span className="text-xs sm:text-base font-semibold">
+                                <span className="text-[10px] sm:text-sm font-semibold">
                                   {item.total_price}/-
                                 </span>
                               </td>
@@ -217,15 +222,15 @@ const OrderDetails = ({ onClick }) => {
               </div>
             </div>
           </div>
-          <div className="absolute bottom-0 flex flex-col gap-5">
+          <div className="absolute bottom-0 flex flex-col gap-2 sm:gap-5">
             <div className="flex justify-between px-4 sm:px-10 w-full mt-8">
-              <div className="text-xs sm:text-xl lg:text-2xl  text-slate-500">
+              <div className="text-[10px] sm:text-sm lg:text-xl  text-slate-500">
                 [<span className="font-bold text-primary">Note:</span>{" "}
                 <span className="text-slate-500 font-semibold">
                   {singleOrder?.customer_details?.note}]
                 </span>
               </div>
-              <div className="flex flex-col w-1/2 border-t sm:border-t-2 text-xs sm:text-xl lg:text-2xl">
+              <div className="flex flex-col w-1/2 border-t sm:border-t-2 text-[10px] sm:text-base lg:text-2xl">
                 <div className="flex w-full px-4 justify-between">
                   <h1 className="font-mono font-medium">Sub-Total:</h1>
                   <h1 id="subTotal" className="text-title font-mono">
@@ -245,7 +250,7 @@ const OrderDetails = ({ onClick }) => {
                   </h1>
                 </div>
                 <div className="flex w-full px-4 justify-between">
-                  <h1 className="text-sm sm:text-xl font-mono ">Discount: </h1>
+                  <h1 className=" font-mono ">Discount: </h1>
                   <h1 id="discount" className="text-title font-mono">
                     -{singleOrder?.discount}/-
                   </h1>
@@ -256,7 +261,7 @@ const OrderDetails = ({ onClick }) => {
                     -{singleOrder?.customer_details?.paidAmount}/-
                   </h1>
                 </div>
-                <div className="flex w-full px-4 py-1 justify-between rounded-sm bg-sub">
+                <div className="flex w-full px-4 sm:py-1 justify-between rounded-sm bg-sub">
                   <h1 className="font-mono font-bold">Total Due: </h1>
                   <h1 id="total" className="font-bold text-primary font-mono">
                     {singleOrder?.dueAmount}.00/-
